c7684e598e44f5fd1795e7ba299f528a826ad836
kumartechie <jkcode87>
2022-04-01 13:29:08 -0500

    Initial Commit

--- src/main/java/com/example/springcertification/SpringcertificationApplication.java
+++ src/main/java/com/example/springcertification/SpringcertificationApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringcertificationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringcertificationApplication.class, args);
+		System.out.println("Spring Cert Project ");
 	}
 
 }
