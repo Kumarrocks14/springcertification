17959566ce390c318d7989a8db13f4bb867c89a5
kumartechie <jkcode87>
2022-04-01 13:26:22 -0500

    Initial commit

---
+++ src/main/java/com/example/springcertification/SpringcertificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcertification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcertificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcertification/SpringcertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcertification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
