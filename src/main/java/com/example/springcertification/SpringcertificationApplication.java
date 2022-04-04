5614a10c8ead0beaf68f03e4bdecd8d366f58734
kumartechie <jkcode87>
2022-04-04 11:08:40 -0500

    Application & CommandLineRunner

--- src/main/java/com/example/springcertification/SpringcertificationApplication.java
+++ src/main/java/com/example/springcertification/SpringcertificationApplication.java
@@ -1,15 +1,28 @@
-package com.example.springcertification;
 
+
+package com.example.springcertification;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcertificationApplication {
-
+public class SpringcertificationApplication implements CommandLineRunner, ApplicationRunner
+{
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringcertificationApplication.class, args);
 		System.out.println("Spring Cert Project ");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Inside CommandLine Runner run method");
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("Inside Application Runner run method");
+	}
 }
